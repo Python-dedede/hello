6c2b5167cbcd53951771621b5c052beaa9db4665
yuhs <3188340143@qq.com>
2019-07-19 19:02:51 +0800

    工程项目创建日

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+/**
+ * @Classname Hello
+ * @Description TODO
+ * @Date 2019/7/19 18:58
+ * @Created by yuhousheng
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
